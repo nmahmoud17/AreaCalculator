0024fa06fb67a56ae51649c52b4704270312e25c
Nada Mahmoud <nadamahmoud@Nadas-MacBook-Pro.local>
2019-01-22 15:10:10 -0500

    Testing Calc

--- src/com/company/Calculator.java
+++ src/com/company/Calculator.java
@@ -2,7 +2,29 @@ package com.company;
 
 public class Calculator {
 
-    private String width;
-    private String height;
+    private int width;
+    private int height;
+
+    public void setWidth (int recWidth) {
+        width = recWidth;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public void setHeight(int recHeight) {
+        height = recHeight;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public  int calculateArea(int height, int width) {
+        int area = height * width;
+        return area;
+    }
+
 
 }
--- src/com/company/CalculatorDemo.java
+++ src/com/company/CalculatorDemo.java
@@ -4,6 +4,12 @@ public class CalculatorDemo {
 
     public static void main(String[] args) {
 
+        Calculator myArea = new Calculator ();
+
+        myArea.setHeight(5);
+        myArea.setWidth(10);
+
+        System.out.println ("The rectangle's area is " + myArea.calculateArea(myArea.getHeight(), myArea.getWidth()));
 
     }
 }
